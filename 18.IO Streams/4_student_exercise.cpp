adb955336f62596597ea6aea095919731b32cf2e
afkniladri <niladri1406@gmail.com>
2021-06-16 00:39:57 +0530

    feat : corrections made

--- 18.IO Streams/4_student_exercise.cpp
+++ 18.IO Streams/4_student_exercise.cpp
@@ -2,44 +2,74 @@
 #include<fstream>
 using namespace std;
 
-class Product{
-    public:
+class Item{
+    private:
         string name;
         float price;
-        int quantity;
+        int qty;
+    public:
+        Item(){}
+        Item(string n,float p,int q);
+        friend ifstream & operator>>(ifstream &ifs,Item &i);
+        friend ofstream & operator<<(ofstream &ofs,Item &i);
+        friend ostream & operator<<(ostream &out,Item &i);
 
-        //constructor
-        Product(string name = " ",float price = 0, int quantity = 0){
-            this->name = name;
-            this->price = price;
-            this->quantity = quantity;
-        }
-
-        friend ifstream & operator>>(ifstream &ifs, Product &p);
-        friend ofstream & operator<<(ofstream &ofs, Product &p);
 };
 
-ifstream & operator>>(ifstream &ifs, Product &p){
-    ifs>>p.name>>p.price>>p.quantity;
-    return ifs;
+Item::Item(string n,float p,int q){
+    name=n;
+    price=p;
+    qty=q;
 }
 
-ofstream & operator<<(ofstream &ofs, Product &p){
-    cout<<"Name     : "<<p.name<<endl;
-    cout<<"Price    : $"<<p.price<<endl;
-    cout<<"Quantity : "<<p.quantity<<endl;
+ofstream & operator<<(ofstream &ofs,Item &i){
+    ofs<<i.name<<endl<<i.price<<endl<<i.qty<<endl;
     return ofs;
 }
 
-int main(){
-    Product p1("Hennessey Cognac",399.99,60);
+ifstream & operator>>(ifstream &ifs,Item &i){
+    ifs>>i.name>>i.price>>i.qty;
+    return ifs;
+}
+
+ostream & operator<<(ostream &out,Item &i){
+    out<<i.name<<endl<<i.price<<endl<<i.qty<<endl;
+    return out;
+}
+
+
+int main()
+{
+    int n;
+    string name;
+    float price;
+    int qty;
+
+    cout<<"Enter number of Item:";
+    cin>>n;
+
+    Item *list[n];
+
+    cout<<"Enter All Item "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<"Enter "<<i+1<<" Item Name , Price and Quantity"<<endl;
+        cin>>name;
+        cin>>price;
+        cin>>qty;
+
+        list[i]=new Item(name,price,qty);
+    }
+
+    ofstream fos("product.txt");
+    for(int i=0;i<n;i++){
+        fos<<*list[i];
+    }
+    
+    Item item;
     ifstream ifs("product.txt");
-    ifs>>p1;
-    ifs.close();
-
-    ofstream ofs("product.txt");
-    ofs<<p1;
-    ofs.close();
-
+    for(int i=0;i<n;i++){
+        ifs>>item;
+        cout<<"Item "<<i<<endl<<item<<endl;
+    }
     return 0;
 }
